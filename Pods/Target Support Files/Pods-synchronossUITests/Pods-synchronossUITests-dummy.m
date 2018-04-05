e4309e3764b2fadb63711ae703ad420f14b8895a
Lucas Correa <contato@lucascorrea.com>
2018-04-05 15:39:53 +0100

    Added cocoapods Alamofire and Swiftlint

---
+++ Pods/Target Support Files/Pods-synchronossUITests/Pods-synchronossUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_synchronossUITests : NSObject
+@end
+@implementation PodsDummy_Pods_synchronossUITests
+@end
---
+++ synchronoss/Networking/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  synchronoss
+//
+//  Created by Lucas Correa on 05/04/2018.
+//  Copyright © 2018 SiriusCode. All rights reserved.
+//
+
+import Foundation
